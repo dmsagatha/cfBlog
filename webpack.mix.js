b1c03260d91ed27b7848df92dfee7213b2f7a798
Doris Morales <dmorales55@gmail.com>
2021-04-29 12:55:27 -0500

    Blog con Laravel, Tailwindcss y Alpine

---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        require('postcss-import'),
+        require('tailwindcss'),
+    ]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.browserSync({
+    proxy: "http://cfBlog.test/",
+    browser: "Google Chrome",
+    open: false,
+});
+
+mix.disableNotifications();
