11f35a1bb685e2d46e01038891e76237708e47f4
Doris Morales <dmorales55@gmail.com>
2021-04-29 17:52:26 -0500

    Seeders y Factories

---
+++ database/factories/ImageFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Image;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ImageFactory extends Factory
+{
+  protected $model = Image::class;
+  
+  public function definition()
+  {
+    return [
+    ];
+  }
+}
