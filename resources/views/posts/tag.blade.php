47d8170255fb04a4d43649aad3fe26891d3ca74f
Doris Morales <dmorales55@gmail.com>
2021-05-02 15:31:11 -0500

    Filtrar los Posts de un blog por etiquetas

--- resources/views/posts/tag.blade.php
+++ resources/views/posts/tag.blade.php
@@ -1,9 +1,11 @@
 <x-app-layout>
   <div class="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 py-8">
-    <h1 class="uppercase text-center text-3xl font-bold">{{ $category->name }}</h1>
-    
+    <h1 class="uppercase text-center text-3xl font-bold">
+      Etiqueta: {{ $tag->name }}
+    </h1>
+
     @foreach ($posts as $post)
-      <x-card-posts :post="$post" />
+    <x-card-posts :post="$post" />
     @endforeach
 
     <div class="mt-4">
