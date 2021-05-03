bb6578db4ca6b1fcc3acafd3211ae5aeb826e996
Doris Morales <dmorales55@gmail.com>
2021-05-03 16:24:05 -0500

    Crud de Etiquetas

---
+++ resources/views/admin/tags/index.blade.php
@@ -0,0 +1,56 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+  <h1>Lista de Etiquetas</h1>
+@stop
+
+@section('content')
+  @if (session('danger'))
+    <div class="alert alert-danger">
+      <strong>{{  session('danger') }}</strong>
+    </div>
+  @endif
+  @if (session('info'))
+    <div class="alert alert-success">
+      <strong>{{  session('info') }}</strong>
+    </div>
+  @endif
+
+  <div class="card">
+    <div class="card-hear">
+      <a href="{{ route('admin.tags.create') }}" class="btn btn-secondary">Agregar Etiqueta</a>
+    </div>
+    <div class="card-body">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th colspan="2">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($tags as $tag)
+            <tr>
+              <td scope="row">{{ $tag->id }}</td>
+              <td>{{ $tag->name }}</td>
+              <td width="10px">
+                <a href="{{ route('admin.tags.edit', $tag) }}" class="btn btn-primary btn-sm">Editar</a>
+              </td>
+              <td width="10px">
+                <form action="{{ route('admin.tags.destroy', $tag) }}" method="POST">
+                  @csrf
+                  @method('delete')
+
+                  <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                </form>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
+@stop
--- routes/admin.php
+++ routes/admin.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\TagController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', [HomeController::class, 'index'])->name('admin.home');
 
 Route::resource('categorias', CategoryController::class)
-        ->parameters(['categorias' => 'category'])
-        ->names('admin.categories');
+  ->parameters(['categorias' => 'category'])
+  ->names('admin.categories');
+Route::resource('etiquetas', TagController::class)
+  ->parameters(['etiquetas' => 'tag'])
+  ->names('admin.tags');
