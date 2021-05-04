0b85c8bfa00ff9a6ecf0ae65903917ef7a3b0afe
Doris Morales <dmorales55@gmail.com>
2021-05-04 15:16:48 -0500

    Subir imagenes al servidor

--- resources/views/components/card-posts.blade.php
+++ resources/views/components/card-posts.blade.php
@@ -1,7 +1,11 @@
 @props(['post'])
 
 <article class="mb-8 bg-white shadow-lg rounded-lg overflow-hidden">
-  <img class="w-full h-72 object-cover object-center" src="{{ Storage::url($post->image->url) }}" alt="">
+  @if ($post->image)
+    <img class="w-full h-72 object-cover object-center" src="{{ Storage::url($post->image->url) }}" alt="">    
+  @else
+    <img class="w-full h-72 object-cover object-center" src="https://cdn.pixabay.com/photo/2021/04/13/14/22/panther-6175825_960_720.jpg" alt="">
+  @endif
 
   <div class="px-6 py-4">
     <h1 class="font-bold text-xl mb-2">
@@ -9,14 +13,16 @@
     </h1>
 
     <div class="text-gray-700 text-base">
-      {{ $post->extract }}
+      {!! $post->extract !!}
     </div>
   </div>
 
   <div class="px-6 pt-4 pb-2">
     @foreach ($post->tags as $tag)
-    <a href="{{ route('posts.tag', $tag) }}"
-      class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm text-gray-900 mr-2 bg-{{ $tag->color }}-600">{{ $tag->name }}</a>
+      <a href="{{ route('posts.tag', $tag) }}"
+      class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm text-gray-900 mr-2 bg-{{ $tag->color }}-600">
+        {{ $tag->name }}
+      </a>
     @endforeach
   </div>
 </article>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,18 +3,22 @@
     <h1 class="text-4xl font-bold text-gray-500">{{ $post->name }}</h1>
 
     <div class="text-lg text-gray-500 mb-2">
-      {{ $post->extract }}
+      {!! $post->extract !!}
     </div>
     
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
       {{-- Contenido principal --}}
       <div class="lg:col-span-2">
         <figure>
-          <img class="w-full h-80 object-cover object-center" src="{{ Storage::url($post->image->url) }}" alt="">
+          @if ($post->image)
+            <img class="w-full h-80 object-cover object-center" src="{{ Storage::url($post->image->url) }}" alt="">
+          @else
+            <img class="w-full h-80 object-cover object-center" src="https://cdn.pixabay.com/photo/2021/04/13/14/22/panther-6175825_960_720.jpg" alt="">
+          @endif
         </figure>
         
         <div class="text-base text-gray-500 mt-4">
-          {{ $post->body }}
+          {!! $post->body !!}
         </div>
       </div>
       
@@ -28,8 +32,13 @@
           @foreach ($similares as $similar)
             <li class="mb-4">
               <a class="flex" href="{{ route('posts.show', $similar) }}">
-                <img class="w-38 h-20 object-cover object-center" src="{{ Storage::url($similar->image->url) }}" alt="">
-                <span class="ml-2 text-gray-600">{{ $similar->name }}</span>
+                @if ($similar->image)
+                  <img class="w-38 h-20 object-cover object-center" src="{{ Storage::url($similar->image->url) }}" alt="">
+                  <span class="ml-2 text-gray-600">{{ $similar->name }}</span>
+                @else
+                  <img class="w-38 h-20 object-cover object-center" src="https://cdn.pixabay.com/photo/2021/04/13/14/22/panther-6175825_960_720.jpg" alt="">
+                  <span class="ml-2 text-gray-600">{{ $similar->name }}</span>
+                @endif
               </a>
             </li>
           @endforeach
