e425edd16c4406a72099921fbcef2cb1714a98df
Doris Morales <dmorales55@gmail.com>
2021-05-06 15:53:51 -0500

    Proteger rutas de acuerdo al rol

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -12,6 +12,14 @@ use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
+  public function __construct()
+  {
+    $this->middleware('can:admin.posts.index')->only('index');
+    $this->middleware('can:admin.posts.create')->only('create', 'store');
+    $this->middleware('can:admin.posts.edit')->only('edit', 'update');
+    $this->middleware('can:admin.posts.destroy')->only('destroy');
+  }
+  
   public function index()
   {
     $posts = Post::all();
@@ -45,10 +53,6 @@ class PostController extends Controller
     }
 
     return redirect()->route('admin.posts.index', $post)->with('info', 'Post creado con éxito');
-  }
-
-  public function show(Post $post)
-  {
   }
 
   public function edit(Post $post)
--- routes/admin.php
+++ routes/admin.php
@@ -7,16 +7,18 @@ use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\CategoryController;
 
-Route::get('', [HomeController::class, 'index'])->name('admin.home');
+Route::get('', [HomeController::class, 'index'])->middleware('can:admin.home')->name('admin.home');
 
 Route::resource('usuarios', UserController::class)
   ->only(['index', 'edit', 'update'])
   ->parameters(['usuarios' => 'user'])
   ->names('admin.users');
 Route::resource('categorias', CategoryController::class)
+  ->except('show')
   ->parameters(['categorias' => 'category'])
   ->names('admin.categories');
 Route::resource('etiquetas', TagController::class)
+  ->except('show')
   ->parameters(['etiquetas' => 'tag'])
   ->names('admin.tags');
-Route::resource('posts', PostController::class)->names('admin.posts');
+Route::resource('posts', PostController::class)->except('show')->names('admin.posts');
